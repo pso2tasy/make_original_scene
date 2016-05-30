cfe8c2ba8905d6759cd9701caef09b1ede03dd07
pso2tasy <jpso2tasy@gmail.com>
2016-05-31 07:50:53 +0900

    ファイル読み込みをFileReaderからURL.createObjectURLに変更

--- src/js/lib/dod.js
+++ src/js/lib/dod.js
@@ -5,9 +5,11 @@
  * @author pso2tasy
  * @version 0.0.2
  *
+ * canvas: 反映するcanvas
+ * image:  Image Object
  * option:
  *   callback:
- *     drop: drop 処理後に呼ばれるコールバック関数
+ *     drop: drop 処理後に呼ばれるコールバック関数。f(canvas, image, file)
  */
 (function(root, factory) {
     if(typeof exports === 'object') {
@@ -35,15 +37,10 @@
   
       if (!file.type.match(/^image\/(png|jpeg|gif)$/)) return;
   
-      var reader = new FileReader();
-      reader.readAsDataURL(file);
-  
-      reader.onload = function(evt) {
-        image.onload = function() {
-          if(option.callback.onload !== 'undefined') option.callback.onload(canvas, image, file);
-        }
-        image.src = evt.target.result;
+      image.onload = function() {
+        if(option.callback.onload !== 'undefined') option.callback.onload(canvas, image, file);
       }
+      image.src = URL.createObjectURL(file);
     });
   }
   return dragOnDrop;
